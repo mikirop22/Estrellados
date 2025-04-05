d81046b0efa800bf1cfa13685637b0a983a41016
polmf <99polmf@gmail.com>
2025-04-05 12:14:54 +0200

    correcio

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -3,7 +3,7 @@ from .views import login_view, home
 from django.shortcuts import redirect
 
 urlpatterns = [
-    path('', lambda request: redirect('login')),  # redirige raíz a login
+    path('', lambda request: redirect('login')),
     path('login/', login_view, name='login'),
     path('home/', home, name='home'),
 ]
